c8e19aa25164d1600eafc26ad78da11910b21efa
siberia <1322801233@qq.com>
2018-12-13 00:21:28 +0800

    判断icon左右以及css

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,26 @@
 <template>
-    <button class="s-button">按钮</button>
+    <button class="s-button" :class="{[`icon-${icon_position}`]: true,}">
+        <svg v-if="icon" class="icon" aria-hidden="true">
+            <use :xlink:href="`#icon-i-${icon}`"></use>
+        </svg>
+        <slot></slot>
+    </button>
 </template>
 <script>
-    export default {}
+    export default {
+        // props: ['icon', 'icon_position']
+        props: {
+            icon: {},
+            icon_position: {
+                type: String,
+                default: 'left',
+                validator: function (value) {
+                    // 这个值必须匹配下列字符串中的一个
+                    return ['left', 'right'].indexOf(value) !== -1 
+                }
+            },
+        }
+    }
 </script>
 <style lang="scss">
     .s-button {
@@ -13,6 +31,11 @@
         border: 1px solid var(--border-color);
         background: var(--button-bg);
 
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: top;
+       
         &:hover {
         border-color: var(--border-color-hover);
         }
@@ -23,6 +46,14 @@
 
         &:focus {
             outline: none;
+        }
+
+        &.icon-left.content { order: 2; }
+        &.icon-left.icon { order: 1; }
+        
+        &.icon-right {
+            > .content { order: 1; }
+            > .icon { order: 2; margin: 0 0 0 0.5em;}
         }
     }
 </style>
