5c186b1348980b9aa82a2be11b07ae25fb7d7594
siberia <1322801233@qq.com>
2018-12-17 14:19:59 +0800

    row 和 col 组件基本功能

---
+++ src/col.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col" :class="`col-${spen}`"></div>
+</template>
+<script>
+    export default {
+        name: 'Col',
+        props: {
+            spen: [Number, String],
+        },
+    }
+</script>
+<style lang="scss" >
+    .col{
+        height: 100px;
+        background-color: #60afff;
+        width: 50%;
+        outline: 1px solid #007eff;
+    }
+    $class: col-;
+    // loops through 100 times
+    @for $n from 1 through 24 {
+        // for each $col_#{i} 
+        .#{$class}#{$n} {
+            width: ($n / 24) * 100%;
+            // background-color: darken(cornflowerblue, 0% + ($n / 2));
+        }
+    }
+</style>
---
+++ src/row.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="s-row">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Row',
+        props: {},
+    }
+</script>
+<style lang="scss" scoped>
+    .s-row{
+        display: flex;
+        // & > .col{
+        //     height: 100px;
+        //     background-color: #60afff;
+        //     // width: 50%;
+        //     /* border: 1px solid red; */
+        //     outline: 1px solid #007eff;
+        // }
+    }
+</style>
