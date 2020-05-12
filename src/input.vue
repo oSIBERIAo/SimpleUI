b3b719b02aadb1c2a67425431f6e67db536a0588
Author Name <Author Email>
2020-05-13 05:51:07 +0800

    跑通单元测试。修复测试svg返回错误

--- src/input.vue
+++ src/input.vue
@@ -1,102 +1,112 @@
 <template>
-    <div class="wrapper" :class="{error}">
-        <input :value="value" class="s-input" :class="{disabled, success, error}" :readonly="readonly" 
-        @input="$emit('input', $event.target.value)"
-        @change="$emit('input_change', $event.target.value)"
-        @focus="$emit('input_focus', $event.target.value)"
-        @blur="$emit('input_blur', $event.target.value)">               
+    <div class="wrapper" :class="{ error }">
+        <input
+            :value="value"
+            class="s-input"
+            :class="{ disabled, success, error }"
+            :readonly="readonly"
+            @input="$emit('input', $event.target.value)"
+            @change="$emit('input_change', $event.target.value)"
+            @focus="$emit('input_focus', $event.target.value)"
+            @blur="$emit('input_blur', $event.target.value)"
+        />
         <template v-if="error">
-            <s-icon v-if="error" name="alert"><s-icon> 
-        </template> 
+            <s-icon v-if="error" name="alert"></s-icon>
+        </template>
         <!-- 奇怪的BUG -->
-        <span v-if="error">{{error}}</span>
+        <span v-if="error">{{ error }}</span>
     </div>
 </template>
 <script>
-    import Icon from './icon'
-    export default {
-        components: {
-            's-icon': Icon,
+import Icon from './icon'
+
+export default {
+    components: {
+        's-icon': Icon,
+    },
+    name: 's-input',
+    props: {
+        name: {},
+        value: {
+            type: String,
         },
-        name: 's-input',
-        props: {
-            name: {},
-            value: {
-                type: String,
-            },
-            error: {
-                type: String,
-            },
-            success: false,
-            disabled: false,
-            readonly: {
-                type: Boolean,
-                default: false
-            },
+        error: {
+            type: String,
         },
-    }
+        success: false,
+        disabled: false,
+        readonly: {
+            type: Boolean,
+            default: false,
+        },
+    },
+}
 </script>
 <style lang="scss" scoped>
-  $height: 32px;
-  $border-color: #2C95FF;
-  $border-color-hover: #666;
-  $border-radius: 4px;
-  $font-size: 12px;
-  $box-shadow-color: rgba(0, 0, 0, 0.5);
-  $red: #F1453D;
-.wrapper { 
+$height: 32px;
+$border-color: #2c95ff;
+$border-color-hover: #666;
+$border-radius: 4px;
+$font-size: 12px;
+$box-shadow-color: rgba(0, 0, 0, 0.5);
+$red: #f1453d;
+.wrapper {
     position: relative;
     font-size: $font-size;
-    display: inline-flex; 
-    align-items: center; 
+    display: inline-flex;
+    align-items: center;
+
     & > .s-icon {
-        fill: #F03D3D;
+        fill: #f03d3d;
         margin-left: -2em;
     }
+
     & > span {
         font-size: 14px;
-        position: absolute; 
+        position: absolute;
         margin-top: 24px;
         margin-left: 10px;
         padding-top: 4px;
-        color: #F03D3D;
+        color: #f03d3d;
     }
 }
 .s-input {
     display: inline-flex;
     justify-content: center;
     align-items: center;
-    vertical-align: top;  // 修复对齐Bug
-    height: 32px; 
-    border: 2px solid #E1E4E8;
-    border-radius: 4px; padding: 0 8px; 
+    vertical-align: top; // 修复对齐Bug
+    height: 32px;
+    border: 2px solid #e1e4e8;
+    border-radius: 4px;
+    padding: 0 8px;
     font-size: inherit;
-    // 
+    //
     &:hover {
-        border: 2px solid #98A1B1;
+        border: 2px solid #98a1b1;
     }
 
     &:active {
-        border: 2px solid #2C95FF;
+        border: 2px solid #2c95ff;
     }
 
     &:focus {
         outline: none;
-        border: 2px solid #2C95FF;
+        border: 2px solid #2c95ff;
     }
 
     &.error {
-        border: 2px solid #F03D3D;
-    }
-    &.success{
-        border: 2px solid #3ED3A3;
-    }
-    &.disabled{
-        border-color: #E1E4E8;
-        color: #E1E4E8;
-        background: #E1E4E8;
-        cursor: not-allowed;
+        border: 2px solid #f03d3d;
     }
 
-} 
+    &.success {
+        border: 2px solid #3ed3a3;
+    }
+
+    &.disabled {
+        border-color: #e1e4e8;
+        color: #e1e4e8;
+        background: #e1e4e8;
+        cursor: not-allowed;
+    }
+}
 </style>
